4d2984d0e71493699893b42ddf5efb472db30b22
Victor Laurentino <victorlaurentino7@gmail.com>
2022-02-28 16:36:03 -0300

    Update posts

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -24,8 +24,6 @@ export default function Blog() {
     },
   });
 
-  console.log("Page", page);
-
   return (
     <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
       <div className="absolute inset-0">
